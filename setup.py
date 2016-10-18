5389d9948a3269e41e5be6795fdd9c94eb80a1c0
Patrick Michaud <pmichaud@uw.edu>
2016-10-18 13:14:48 -0700

    Moving to a matrix of django versions...

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup, find_packages
+import os
+
+install_requires = ['Django']
+if os.environ.get("DJANGO_VERSION"):
+    install_requires = ['Django=%s' % os.environ.get("DJANGO_VERSION")]
 
 setup(
     name='AuthZ-Group',
     version='1.2.0',
     description='Group interface and implementations',
-    install_requires=['Django'],
+    install_requires=install_requires,
     packages=find_packages(exclude=["project"]),
     include_package_data=True,  # use MANIFEST.in during install
     author = "Patrick Michaud",
